16fe48f481fb679cbefe8586b6ffbc025b29ebd2
Sergey Kozhevnikov <SYKozhevnikov@rasu.ru>
2024-02-27 23:50:44 +0300

    Задание 4

--- MyServer.js
+++ MyServer.js
@@ -1,8 +1,7 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-    
-    
+       
     console.log('Url: ' + request.url);
     console.log('Тип запроса: ' + request.method);
     console.log('User-Agent: ' + request.headers['user-agent']);
@@ -16,8 +15,11 @@ http.createServer(function (request, response) {
     response.write('<title>Мой первый сервер на Note.JS</title>');
     response.write('<meta charset="utf-8" />');
     response.write('</head>');
+    response.write('<script src="app.js" async></script>');
     response.write('<body><h2>Мой первый сервер на Note.JS</h2></body>');
     response.write('</html>');
 
     response.end();
+
+
 }).listen(3000);
