4a6e8bae8c166d80aede3bd7227fb8454a2937ca
Sergey Kozhevnikov <SYKozhevnikov@rasu.ru>
2024-03-06 00:35:49 +0300

    Часть 2

---
+++ public/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let name1 = 'Денис';
+name1.toLowerCase();
+console.log(name1);
+let arr = [1, 3, 5, 7];
+arr.pop();
+console.log(arr);
+name1 = 'Анна';
+//# sourceMappingURL=index.js.map
